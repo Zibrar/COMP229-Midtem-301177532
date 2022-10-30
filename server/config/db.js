88f67e87576b974374ffb2c2e56a79604a11a5dd
Zarik Ibrar <zarikhassan@gmail.com>
2022-10-30 00:36:18 -0400

    first

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  URI: "mongodb+srv://Zack69:Pakistan123@cluster0.zwncsh6.mongodb.net/?retryWrites=true&w=majority",
+  Secret: "SomeSecret",
 };
